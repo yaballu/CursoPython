da19b7185432d1d987184433b169a326bd3fed29
Ket <ketelilino@gmail.com>
2024-05-03 02:00:04 -0300

    novas coisas

--- Fundamentos/Metodos_dicionarios.py
+++ Fundamentos/Metodos_dicionarios.py
@@ -70,3 +70,74 @@ contatos = {
 
 print(contatos.pop("keteli@gmail.com"))
 print(contatos.pop("keteli@gmail.com", {}))
+
+# popitem
+
+contatos = {
+    "keteli@gmail.com": {"nome": "Keteli", "idade": 17},
+}
+# print(contatos.popitem())
+# print(contatos.popitem())
+
+# setdefault
+
+contatos = {
+    "keteli@gmail.com": {"nome": "Keteli", "idade": 17},
+}
+
+contatos.setdefault("nome", "anabelle")
+print(contatos)
+
+contatos.setdefault("especialidade", "cozinheira")
+print(contatos)
+
+# update
+
+contatos = {
+    "keteli@gmail.com": {"nome": "Keteli", "idade": 17},
+}
+
+contatos.update({"keteli@gmail.com": {"nome":"Ket"}})
+print(contatos)
+
+contatos.update({"anabelle@gmail.com": {"nome": "belle", "idade": 16}})
+print(contatos)
+
+# values
+
+contatos = {
+    "keteli@gmail.com": {"nome": "Ket", "idade": 17},
+    "anabelle@gmail.com": {"nome": "ana", "idade": 16},
+    "roxanne@gmail.com": {"nome": "roxanne", "idade": 23},
+    "dalia@gmail.com": {"nome": "dalia", "idade": 30},
+}
+
+print(contatos.values())
+
+# in
+
+contatos = {
+    "keteli@gmail.com": {"nome": "Ket", "idade": 17},
+    "anabelle@gmail.com": {"nome": "ana", "idade": 16},
+    "roxanne@gmail.com": {"nome": "roxanne", "idade": 23},
+    "dalia@gmail.com": {"nome": "dalia", "idade": 30},
+}
+
+print("keteli@gmail.com" in contatos)
+print("coco@gmail.com" in contatos)
+print("idade" in contatos["keteli@gmail.com"])
+print("telefone" in contatos["anabelle@gmail.com"])
+
+# del
+
+contatos = {
+    "keteli@gmail.com": {"nome": "Ket", "idade": 17},
+    "anabelle@gmail.com": {"nome": "ana", "idade": 16},
+    "roxanne@gmail.com": {"nome": "roxanne", "idade": 23},
+    "dalia@gmail.com": {"nome": "dalia", "idade": 30},
+}
+
+del contatos["keteli@gmail.com"]["idade"]
+del contatos["roxanne@gmail.com"]
+
+print(contatos)
